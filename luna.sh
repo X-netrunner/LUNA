fa1fc79c6740e6486822405ea1184ea4449d4004
X-netrunner <srijanbandaru@gmail.com>
2026-02-20 22:10:19 +0530

    Initial commit: Luna v2.3.1 #Prev updates -Fast routing updated -REMOVED 1.5B model due to unstabilty and unreliabbility as an agentic tasks
    #Current Update
    
    -The outputs are more consistant and actually works now.
    -Routing between models is fixed.
    -Model status stored in logs.
    -Added 1.5B to work as an future router between 3B and 8B

--- luna.sh
+++ luna.sh
@@ -5,6 +5,7 @@ set -euo pipefail # was getting some weird error without this
 
 f_p=$(pwd) #path of files
 RAG="$f_p/memory/rag.py"
+MODEL_SMALL="qwen2:1.5b-instruct"
 MODEL_MEDIUM="llama3.2:3b-instruct-q4_k_m"
 MODEL_LARGE="mannix/llama3.1-8b-lexi:q4_k_m"
 LUNA_MODE="ephemeral"   # change to daemon when needed
@@ -17,7 +18,7 @@ LONG_MEMORY="$f_p/memory/long_term.log" #will use later to make sure LUNA will s
 CRITICAL_FILES=("$f_p/luna.sh" "$f_p/prompts/system.txt") #personallities , rules and what not
 
 SCRATCHPAD="" #think about it like cache but for LUNA it will help to keep some data relavent till the session ends.
-DEBUG_MODE=false #if you turn it on you will see SCRATCHPAD data. tbh its not working well right now
+DEBUG_MODE=true #if you turn it on you will see SCRATCHPAD data. tbh its not working well right now
 export LUNA_DEBUG=$DEBUG_MODE #sends this to rag.py
 
 #User based variables
